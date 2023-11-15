1acd04157f97e1832a1937abeae106a82926dd45
Carera12 <silvicarera123@gmail.com>
2023-11-15 16:25:07 +0700

    Menambah bagian orderUiState

--- app/src/main/java/com/example/esjumbo/data/OrderUIState.kt
+++ app/src/main/java/com/example/esjumbo/data/OrderUIState.kt
@@ -1,6 +1,9 @@
 package com.example.esjumbo.data
 
 data class OrderUIState(
+    val name: String = "",
+    val alamat: String = "",
+    val noTelp: String = "",
     val jumlah: Int = 0,
     val rasa: String = "",
     val harga: String = ""
